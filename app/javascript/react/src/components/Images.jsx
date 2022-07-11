f74bcf102f41c71d041f09f20a5e3c481f6d0093
Regina Jackson <rjackson156@gmail.com>
2022-07-11 19:46:44 -0400

    using correct images instead of placeholders

--- app/javascript/react/src/components/Images.jsx
+++ app/javascript/react/src/components/Images.jsx
@@ -4,9 +4,9 @@ import * as ReactDOM from 'react-dom'
 const Images = () => {                                   
     return (
     <div class="flex-col">
-        <img src="../images/dress.jpg" width='400' height='500' />
-        <img src="../images/skirt.jpg" width='400' height='500'/>
-        <img src="../images/skirt.jpg" width='400' height='500'/>
+        <img src="../images/DSC04218.jpg" width='400' height='500' />
+        <img src="../images/DSC04360.jpg" width='400' height='500'/>
+        <img src="../images/DSC04408.jpg" width='400' height='500'/>
     </div>)                  
   }  
 
