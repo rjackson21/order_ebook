2d786ebc98ca28568738ac68ba53b5cf167dfd23
Regina Jackson <rjackson156@gmail.com>
2022-07-12 17:57:17 -0400

    implement use of tailwind element width classes

--- app/javascript/react/src/components/Images.jsx
+++ app/javascript/react/src/components/Images.jsx
@@ -3,10 +3,10 @@ import * as ReactDOM from 'react-dom'
                                                         
 const Images = () => {                                   
     return (
-    <div class="flex-col ml-4">
-        <img src="../images/DSC04218.jpg" width='400' height='500' />
-        <img src="../images/DSC04360.jpg" width='400' height='500'/>
-        <img src="../images/DSC04408.jpg" width='400' height='500'/>
+    <div>
+        <img src="../images/DSC04218.jpg" width='250' height='300' />
+        <img src="../images/DSC04360.jpg" width='250' height='300'/>
+        <img src="../images/DSC04408.jpg" width='250' height='300'/>
     </div>)                  
   }  
 
--- app/javascript/react/src/components/SalesPage.jsx
+++ app/javascript/react/src/components/SalesPage.jsx
@@ -5,9 +5,17 @@ import Images from "./Images"
 
 export default function SalesPage() {
   return (
-    <div>
-      <Images />
-      <Ipad />
+    <div class="flex w-full">
+      <div class="flex w-1/3">
+        <Images />
+      </div>
+      <div class="flex w-1/3">
+        <Ipad />
+      </div>
+      <div class="flex w-1/3">
+        <p> Blah text </p>
+      </div>
+      
       
     </div>
   )
