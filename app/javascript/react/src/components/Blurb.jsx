d935b94b133af3cdf2d3f631c116ab90c3142c70
Regina Jackson <rjackson156@gmail.com>
2022-07-13 09:04:21 -0400

    change blurb

--- app/javascript/react/src/components/Blurb.jsx
+++ app/javascript/react/src/components/Blurb.jsx
@@ -3,8 +3,10 @@ import * as ReactDOM from 'react-dom'
                                                         
 const Blurb= () => {                                   
   return (
-    <p>A modest collection of 19 recipes that will make going vegan much easier. I always tell people that the easiest way to stick to a new vegan diet is to get in the kitchen and cook! My goal for this book is to get you to do so.</p>
-
+    <p> A modest collection of 19 recipes that will make going vegan much easier. 
+        The easiest way to stick to a new vegan diet is to get in the kitchen and cook! 
+        The goal of this book is to get you to do so.
+    </p>
     )                   
 }                                        
 // Use it if you don't plan to use "remount"                
