2a53871101c7f79746ba82c7b47c91fc91cf28e4
Regina Jackson <rjackson156@gmail.com>
2022-07-12 21:21:32 -0400

    correct column width

--- app/javascript/react/src/components/SalesPage.jsx
+++ app/javascript/react/src/components/SalesPage.jsx
@@ -4,20 +4,28 @@ import Ipad from "./Ipad"
 import ImageB from "./ImageB"
 import ImageA from './ImageA'
 import ImageC from './ImageC'
+import Buy from './Buy'
 
 export default function SalesPage() {
   return (
     <div class="flex w-full">
-      <div class="flex flex-col pt-10 w-1/3">
-        <ImageA />
-        <ImageB />
-        <ImageC />
+      <div class="flex flex-col p-10 w-1/4">
+        <div class="flex">
+          <ImageA />
+        </div>
+        <div class="flex">
+          <ImageB />
+        </div>
+        <div class="flex">
+          <ImageC />
+        </div>
       </div>
-      <div class="flex w-1/3">
+      <div class="flex flex-col justify-center w-1/2">
         <Ipad />
       </div>
-      <div class="flex w-1/3">
+      <div class="flex flex-col justify-center w-1/4">
         <p> Blah text </p>
+        <Buy />
       </div>
       
       
