ca8493df54e8dd9d6de8e6ee6ded0c857803754c
Regina Jackson <rjackson156@gmail.com>
2022-07-12 21:20:12 -0400

    import and define buy component

--- app/javascript/react/src/index.js
+++ app/javascript/react/src/index.js
@@ -3,10 +3,12 @@ import ImageA from "./components/ImageA"
 import ImageB from "./components/ImageB"
 import ImageC from "./components/ImageC"
 import Ipad from "./components/Ipad"
+import Buy from "./components/Buy"
 import SalesPage from './components/SalesPage'
-                                      
+
 define({ 'imagea-component': ImageA })      
 define({ 'imageb-component': ImageB })      
 define({ 'imagec-component': ImageC })                           
 define({ 'ipad-component' : Ipad }) 
+define({ 'buy-component': Buy }) 
 define({ 'sales-page'     : SalesPage })
